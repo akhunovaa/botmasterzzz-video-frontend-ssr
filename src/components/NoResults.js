7fcdf99d1ece31aae43ddeb6352421ae834b011c
Azat Akhunov <azat.akhunov@protonmail.com>
2020-12-03 23:24:20 +0300

    trending and search results added

--- src/components/NoResults.js
+++ src/components/NoResults.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "../sass/NoResults.css"
 
-const NoResults = ({ title, text }) => {
-  return (
-    <div className='no-result'>
-      <img className='no-result-img' src={"https://video.yourapi.ru/default.jpeg"} alt="Результатов нет..." />
-      <h2>{title}</h2>
-      <p className="secondary">{text}</p>
-    </div>
-  );
+const NoResults = ({title, text}) => {
+    return (
+        <div className='no-result'>
+            <a href="https://video.yourapi.ru">
+                <img className='no-result-img' src={"https://video.yourapi.ru/default.jpeg"} alt="Ничего не найдено..."/>
+            </a>
+            <h2>{title}</h2>
+            <a href="https://t.me/tiktiktokrobot">
+                <p className="secondary">{text}</p>
+            </a>
+
+        </div>
+    );
 };
 
 export default NoResults;
