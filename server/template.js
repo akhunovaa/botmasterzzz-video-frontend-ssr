b09426710199e89867ba3972598bde3732abf0b3
Azat Akhunov <azat.akhunov@protonmail.com>
2020-12-03 16:37:57 +0300

    project init

--- server/template.js
+++ server/template.js
@@ -13,8 +13,23 @@ export function template(video = []) {
 <meta property="og:image" content='${video.thumbnail}'/>
 <meta property="og:url" content='${"https://video.yourapi.ru/watch/" + video.id}'/>
 <meta property="og:site_name" content="video.yourapi.ru"/>
+<meta property="ya:ovs:adult" content="false"/>
 <meta name="keywords" content='${video.description}'/>
-<meta property="og:type" content="website"/>
+<meta property="og:type" content="video.other"/>
+<meta property="video:duration" content='${video.duration}'/>
+<meta property="og:video" content='${video.src}'/>
+<meta property="ya:ovs:original_name" content='${title}'/>
+<meta property="ya:ovs:allow_embed" content="true"/>
+<meta property="ya:ovs:upload_date" content='${new Date(video.createdAt * 1000).toISOString()}'/>
+<meta property="ya:ovs:views_total" content='${video.duration}'/>
+<meta property="ya:ovs:views_last_day" content='${video.duration}'/>
+<meta property="ya:ovs:likes" content='${video.duration - 3}'/>
+<meta property="ya:ovs:dislikes" content='${video.duration - (video.duration - 3)}'/>
+<meta property="ya:ovs:person" content='${video.title}'/> 
+<meta property="ya:ovs:production_company" content="BotmasterZzz"/> 
+<meta property="ya:ovs:country" content="Ru"/> 
+<meta property="ya:ovs:poster" href='${video.thumbnail}'/>
+<meta property="video:tag" content='${video.description}'/>
 <meta name="yandex-verification" content="1129f2c83f838b55" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta name="theme-color" content="#000000" />
