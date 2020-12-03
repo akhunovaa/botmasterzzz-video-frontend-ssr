cdf483acca67985652e0a6ca1e9adb896b3f8071
Azat Akhunov <azat.akhunov@protonmail.com>
2020-12-03 17:20:54 +0300

    project init

--- server/template.js
+++ server/template.js
@@ -39,15 +39,15 @@ export function template(video = []) {
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta name="theme-color" content="#000000" />
 
-     <meta name="twitter:card" content="player">
-    <meta name="twitter:site" content="@yourapi_ru">
-    <meta name="twitter:url" content='${"https://video.yourapi.ru/watch/" + video.id}'>
-    <meta name="twitter:title" content='${title}'>
-    <meta name="twitter:description" content='${video.description}'>
+     <meta name="twitter:card" content="player"/>
+    <meta name="twitter:site" content="@yourapi_ru"/>
+    <meta name="twitter:url" content='${"https://video.yourapi.ru/watch/" + video.id}'/>
+    <meta name="twitter:title" content='${title}'/>
+    <meta name="twitter:description" content='${video.description}'/>
     <meta name="twitter:image" content='${video.thumbnail}'>
-    <meta name="twitter:player" content='${video.src}'>
-     <meta name="twitter:player:width" content="1280">
-     <meta name="twitter:player:height" content="720">
+    <meta name="twitter:player" content='${video.src}'/>
+     <meta name="twitter:player:width" content="1280"/>
+     <meta name="twitter:player:height" content="720"/>
       
 <script src="https://unpkg.com/videojs-contrib-hls/dist/videojs-contrib-hls.js"></script>
 <link rel="apple-touch-icon" href="./logo192.png" />
