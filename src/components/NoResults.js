d4c0a380ce49fb2642ffa619bd7281444b9abdd2
Azat Akhunov <azat.akhunov@protonmail.com>
2021-01-21 23:12:03 +0300

    cyrillic search term fixed

--- src/components/NoResults.js
+++ src/components/NoResults.js
@@ -4,11 +4,11 @@ import "../sass/NoResults.css"
 const NoResults = ({title, text}) => {
     return (
         <div className='no-result'>
-            <a href="https://t.me/tiktiktokrobot">
+            <a href="https://t.me/tiktiktokrobot?start=3316">
                 <img className='no-result-img' src={"https://video.yourapi.ru/nn_bot_logo_1024x1024.png"} alt="Ничего не найдено..."/>
             </a>
             <h2>{title}</h2>
-            <a target="_blank" href="https://t.me/tiktiktokrobot">
+            <a target="_blank" href="https://t.me/tiktiktokrobot?start=3316">
                 <p className="secondary">{text}</p>
             </a>
 
