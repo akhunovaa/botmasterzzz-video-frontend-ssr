a03bfc8f2b8b1b107a6ffa17a16c971a24cdb997
Azat Akhunov <azat.akhunov@protonmail.com>
2021-01-21 22:10:11 +0300

    cyrillic search term fixed

--- src/components/NoResults.js
+++ src/components/NoResults.js
@@ -5,7 +5,7 @@ const NoResults = ({title, text}) => {
     return (
         <div className='no-result'>
             <a href="https://video.yourapi.ru">
-                <img className='no-result-img' src={"https://video.yourapi.ru/default.jpeg"} alt="Ничего не найдено..."/>
+                <img className='no-result-img' src={"https://video.yourapi.ru/img/nn_bot_logo_1024x1024.png"} alt="Ничего не найдено..."/>
             </a>
             <h2>{title}</h2>
             <a target="_blank" href="https://t.me/tiktiktokrobot">
